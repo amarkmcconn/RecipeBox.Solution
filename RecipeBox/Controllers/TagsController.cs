4475ea199ffb8d2fe0ffcf721317a5c2dc53bf66
Mark McConnell <amark.mcconnell@gmail.com>
2022-05-31 22:11:49 -0700

    styling and update views

--- RecipeBox/Controllers/TagsController.cs
+++ RecipeBox/Controllers/TagsController.cs
@@ -4,14 +4,6 @@ using RecipeBox.Models;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Mvc;
-using RecipeBox.Models;
-using System.Linq;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
-using System.Threading.Tasks;
-using System.Security.Claims;
 
 namespace RecipeBox.Controllers
 {
